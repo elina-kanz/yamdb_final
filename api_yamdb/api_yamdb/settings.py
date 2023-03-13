66961810f3ef0dabb1e3e5170025adeb01f2f0d4
Elina Kanzafarova <elkanzafarova@gmail.com>
2023-03-13 17:07:00 +0300

    new workflow

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 dotenv.load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", default='secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
