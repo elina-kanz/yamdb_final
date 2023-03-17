9bf7e3d69f4b86b506ac4cf2880d4aee555ec74f
Elina Kanzafarova <elkanzafarova@gmail.com>
2023-03-17 18:29:23 +0300

    правки

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -68,7 +68,6 @@ WSGI_APPLICATION = "api_yamdb.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': os.getenv(
-
             'DB_ENGINE',
             default='django.db.backends.postgresql'),
         'NAME': os.getenv(
